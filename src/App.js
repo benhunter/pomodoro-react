994b92b46527a70c48a0dc9d49045862b135b859
Ben Hunter <code@benhunter.me>
2023-12-16 18:03:17 -0600

    Bugfix - timer stops at zero. Upgrade react-scripts

--- src/App.js
+++ src/App.js
@@ -3,46 +3,54 @@ import './App.css';
 
 
 function App() {
-  document.title = "Pomodoro Timer"
-  const [isRunning, setIsRunning] = useState(false);
-  const [timeStartPomodoro] = useState(1000 * 60 * 25);
-  const [timeLeft, setTimeLeft] = useState(timeStartPomodoro);  // milliseconds remaining
+    document.title = "Pomodoro Timer"
+    const [isRunning, setIsRunning] = useState(false);
+    const [timeStartPomodoro] = useState(1000 * 5); //1000 * 60 * 25);
+    const [timeLeft, setTimeLeft] = useState(timeStartPomodoro);  // milliseconds remaining
 
-  useEffect(() => {
-    if (!isRunning) return;
-    const timer = setTimeout(() => {
-      setTimeLeft(timeLeft-100);
-    }, 100);
-    // Clear timeout if the component is unmounted
-    return () => clearTimeout(timer);
-  });
-  
-  return (
-    <div className="center">
-      <div 
-        className="timer">
-        {Math.floor((timeLeft / 1000 / 60) % 60).toString().padStart(2, '0')}:
-        {Math.floor((timeLeft / 1000) % 60).toString().padStart(2, '0')}</div>
-      <div>
-        <button
-          className="start-button"
-          onClick={() => setIsRunning(!isRunning)}
-        >
-          {isRunning ? "Pause" : "Start"}
-        </button>
-      </div>
-      <div>
-        <button
-          className="reset-button"
-          onClick={() => {
-            setIsRunning(false)
-            setTimeLeft(timeStartPomodoro)
-          }}>
-          Reset
-        </button>
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        if (!isRunning) return;
+        const timer = setTimeout(() => {
+            setTimeLeft(timeLeft - 100);
+        }, 100);
+
+        if (timeLeft <= 0) {
+            setIsRunning(false);
+            setTimeLeft(0);
+        }
+
+        // Clear timeout if the component is unmounted
+        return () => clearTimeout(timer);
+    });
+
+    function resetTimer() {
+        setIsRunning(false)
+        setTimeLeft(timeStartPomodoro)
+    }
+
+    return (
+        <div className="center">
+            <div
+                className="timer">
+                {Math.floor((timeLeft / 1000 / 60) % 60).toString().padStart(2, '0')}:
+                {Math.floor((timeLeft / 1000) % 60).toString().padStart(2, '0')}</div>
+            <div>
+                <button
+                    className="start-button"
+                    onClick={() => setIsRunning(!isRunning)}
+                >
+                    {isRunning ? "Pause" : "Start"}
+                </button>
+            </div>
+            <div>
+                <button
+                    className="reset-button"
+                    onClick={() => resetTimer()}>
+                    Reset
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
